d0c1acb88f7ce33980149a1aac2fde050f37ce03
Kristian Portillo <portillk@gmail.com>
2020-07-09 23:44:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,8 +2,8 @@ def find_min_in_nested_arrays(src)
   count = 0 
   min_temps = []
   while count < src.length do
-    inner_count = 0 
-    min = 0
+    inner_count = 0
+    min = 100000000000000
     while inner_count < src[count].length do
       if src[count][inner_count] < min
         src[count][inner_count] = min
